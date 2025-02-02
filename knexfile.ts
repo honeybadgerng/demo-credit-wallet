543a4fda585280d8b6f758cd7f7c67932b292893
Onabanji <rajimoshood200482@gmail.com>
2025-02-02 18:47:14 +0100

    Modified knexfile.ts to support MySQL and environment variables

---
+++ knexfile.ts
@@ -0,0 +1,20 @@
+import dotenv from "dotenv";
+import { Knex } from "knex";
+
+dotenv.config();
+
+const config: { [key: string]: Knex.Config } = {
+  development: {
+    client: "mysql2",
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    },
+    migrations: { directory: "./migrations" },
+    seeds: { directory: "./seeders" },
+  },
+};
+
+export default config;
